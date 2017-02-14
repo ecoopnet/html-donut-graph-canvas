524272104ab78e5f7e586cde6f957a7b0ebfcda0
M.Inomata <inomata@tech-vein.com>
2017-02-14 18:13:36 +0900

    ドーナツ型描画を共通化

--- donuts.js
+++ donuts.js
@@ -18,38 +18,39 @@ function DonutsCanvas(){
         if (!this.canvas || !this.canvas.getContext) {
             return false;
         }
-        var length = Math.min(canvas.width, canvas.height) / 2;
-        var center = {x: canvas.width / 2, y: canvas.height / 2 };
-        var innerLength = length * 0.7; // inner circle length.
+
         var ratio = this.ratio;
 
         var ctx = this.canvas.getContext('2d');
         ctx.clearRect(0,0,canvas.width, canvas.height);
 
-        console.log("color: " + color + ", ratio:"+ratio);
+        var color = (this.applyColor).bind(this)();
 
-        var originRadian = -Math.PI / 2;
-        var startAngle = originRadian;
+        var drawDonutImpl = function(ctx, ratio, color) {
+            var length = Math.min(canvas.width, canvas.height) / 2;
+            var center = {x: canvas.width / 2, y: canvas.height / 2 };
+            var innerLength = length * 0.7; // inner circle length.
+
+            // console.log("color: " + color + ", ratio:"+ratio);
+
+            var originRadian = -Math.PI / 2;
+            var startAngle = originRadian;
+
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            var endAngle = Math.PI * 2 * ratio + originRadian;
+            ctx.arc(center.x, center.y, innerLength, startAngle, endAngle, false);
+            ctx.arc(center.x, center.y, length, endAngle, startAngle, true);
+
+            ctx.closePath();
+            ctx.fill();
+        }; // .bind(this);
 
         // // fill empty color donut.
-        ctx.fillStyle = this.emptyColor;
-        ctx.beginPath();
-        ctx.arc(center.x, center.y, innerLength, 0, Math.PI * 2, false);
-        ctx.arc(center.x, center.y, length, Math.PI * 2, 0, true);
-
-        ctx.closePath();
-        ctx.fill();
+        drawDonutImpl(ctx, 1, this.emptyColor);
 
         // // fill ratio color donut arc.
-        var color = (this.applyColor).bind(this)();
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        var endAngle = Math.PI * 2 * ratio + originRadian;
-        ctx.arc(center.x, center.y, innerLength, startAngle, endAngle, false);
-        ctx.arc(center.x, center.y, length, endAngle, startAngle, true);
-
-        ctx.closePath();
-        ctx.fill();
+        drawDonutImpl(ctx, ratio, color);
     }.bind(this);
 
 }
